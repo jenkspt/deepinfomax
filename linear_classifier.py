9a617cb272b14e731e5aae0191baf50464a81b28
Penn Jenks <jenkspt@gmail.com>
2018-12-04 15:56:50 -0800

    feat: generalize eval_encoder function

--- linear_classifier.py
+++ linear_classifier.py
@@ -5,21 +5,29 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
+def get_features(forward_fn, loader, device=torch.device('cpu')):
+    """
+    Args:
+        forward_fn (callable): function for generating features from a batch
+            with `forward` function that returns y, M
+        loader (DataLoader): Generates batched inputs to the encoder
+    Returns:
+        features (torch.Tensor), labels (torch.Tensor)
+    """
+    assert callable(forward_fn)
 
-def get_features(encoder, loader, device=torch.device('cpu')):
     with torch.set_grad_enabled(False):
-        encoder.to(device) 
-        encoder.eval()
         features_lst = []
         labels_lst = []
         for images, labels in loader:
             images = images.to(device) 
 
-            y, M = encoder(images)
-            features_lst.append(y)
+            feat = forward_fn(images)
+            features_lst.append(feat.view(feat.size(0), -1))
             labels_lst.append(labels)
 
         return torch.cat(features_lst, 0), torch.cat(labels_lst, 0)
+
 
 def train_classifier(
         model,
@@ -74,32 +82,59 @@ def eval_classifier(
     return acc
 
 def eval_encoder(
-        encoder, 
-        train_loader, 
-        eval_loader,
-        num_classes,
+        encoder,
+        loaders,
+        eval_model,
         train_epochs=5,
         batch_size=128,
+        feature_layer='y',
         device=torch.device('cpu')):
 
+    """
+    Args:
+        encoder (nn.Module): Encoder function to generate features from,
+            with `forward` function that returns y, M
+        loaders (dict(DataLoader)): dictionary with keys 'valid' and 'train',
+            which contain DataLoaders
+        eval_model (nn.Module): Module to train on encoder features
+        train_epochs (int): Number of training epochs to train eval_model
+        batch_size  (int): Batch size for training eval_model
+        feature_layer (str): One of 'y', 'fc', 'conv' (case insensitive). The
+            layer in the encoder from which to generate features
+        device (torch.device): 'cpu' or 'cuda'
+    Returns
+        (int): Accuracy of the eval_model trained on encoder features
+    """
+    encoder.eval()
+    feature_layer = feature_layer.lower()
+    if feature_layer == 'y':
+        forward_fn = lambda images: encoder(images)[0]
+    elif feature_layer == 'fc':
+        forward_fn = lambda images: encoder.f1(encoder.f0(encoder.C(images)))
+    elif feature_layer == 'conv':
+        # Encoder needs to implement an interface
+        forward_fn = lambda images: encoder.f0(encoder.C(images))
+    else:
+        raise ValueError(f"feature_layer was {feature_layer}" 
+                + "but should be one of 'y', 'fc', 'conv'")
+
     # Train classifier on encoder features
-    train_features, train_labels = get_features(encoder, train_loader, device)
+    train_features, train_labels = get_features(forward_fn, loaders['train'], device)
 
     train_set = TensorDataset(train_features, train_labels)
     train_features_loader = DataLoader(train_set, batch_size, shuffle=True)
 
-    linear_model = nn.Linear(encoder.encoding_size, num_classes)
-    linear_model = train_classifier(
-            linear_model, 
+    eval_model = train_classifier(
+            eval_model, 
             train_features_loader,
             train_epochs,
             device)
 
     # Evaluate classifier
-    eval_features, eval_labels = get_features(encoder, eval_loader, device)
+    eval_features, eval_labels = get_features(forward_fn, loaders['valid'], device)
     eval_set = TensorDataset(eval_features, eval_labels)
     eval_loader = DataLoader(eval_set, batch_size=batch_size, shuffle=False)
-    accuracy = eval_classifier(linear_model, eval_loader, device)
+    accuracy = eval_classifier(eval_model, eval_loader, device)
     return accuracy
 
 if __name__ == "__main__":
@@ -119,7 +154,9 @@ if __name__ == "__main__":
     feat_loader = DataLoader(feat_ds)
 
     # Test get_features
-    features = get_features(Encoder(feature_layer=2), img_loader, device)
+    encoder = Encoder(feature_layer=2)
+    encoder.eval().to(device)
+    features = get_features(lambda x: encoder(x)[0], img_loader, device)
 
     # Test train_classifier
     linear_model = train_classifier(nn.Linear(64, 10), feat_loader, epochs=1, device=device)
@@ -128,10 +165,9 @@ if __name__ == "__main__":
 
     # Test eval_encoder
     acc = eval_encoder(
-            Encoder(feature_layer=2),
-            img_loader,
-            img_loader,
-            num_classes=10,
+            encoder,
+            {'train':img_loader, 'valid':img_loader},
+            linear_model,
             train_epochs=1,
             batch_size=8,
             device=device)
